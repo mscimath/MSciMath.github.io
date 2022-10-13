3e31029d10df59e7ed72d38cace482f63c81e774
mscimath <madejscimath@gmail.com>
2022-10-13 22:02:14 +0200

    testing

--- horizontalScroll.js
+++ horizontalScroll.js
@@ -2,18 +2,26 @@ var windowWidth = window.innerWidth;
 var horizontalLength = document.querySelector(".element-container").scrollWidth;
 var distanceFromTop = document.querySelector(".horizontal-section").offsetTop;
 var scrollDistance = distanceFromTop + horizontalLength - windowWidth;
-
-console.log(scrollDistance);
+/*For Small devices*/
+var distanceFromTopSmall = document.querySelector(".horizontal-section").offsetTop -100;
 
 /*Set the Height of the Horizontal Section*/
 document.querySelector(".horizontal-section").style.height = horizontalLength + "px";
 
 window.onscroll = function() {
     var scrollTop = window.pageYOffset;
+    
+    /*Determine if on Small Device*/
+    if (windowWidth < 400) {
+        
+        if ( (scrollTop-100)  >= (distanceFromTop) && scrollTop <= scrollDistance) {
+            document.querySelector(".element-container").style.transform = 
+            'translateX(-'+(scrollTop - distanceFromTop) + "px)";
+        }
 
-    if (scrollTop >= distanceFromTop && scrollTop <= scrollDistance) {
+    } else if (scrollTop >= distanceFromTop && scrollTop <= scrollDistance ) {
         document.querySelector(".element-container").style.transform = 
         'translateX(-'+(scrollTop - distanceFromTop) + "px)";
-    }
+    };
 
 }
