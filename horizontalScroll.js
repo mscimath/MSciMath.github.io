b9740ea67368830ebfe792b129f7d8e9c70443c4
mscimath <madejscimath@gmail.com>
2022-10-13 21:01:03 +0200

    added horizontal scroll on physics

---
+++ horizontalScroll.js
@@ -0,0 +1,21 @@
+var windowWidth = window.innerWidth;
+var horizontalLength = document.querySelector(".element-container").scrollWidth;
+var distanceFromTop = document.querySelector(".horizontal-section").offsetTop;
+var scrollDistance = distanceFromTop + horizontalLength - windowWidth;
+
+console.log(scrollDistance);
+
+/*Set the Height of the Horizontal Section*/
+document.querySelector(".horizontal-section").style.height = horizontalLength + "px";
+
+window.onscroll = function() {
+    var scrollTop = window.pageYOffset;
+
+    console.log(windowWidth);
+
+    if (scrollTop >= distanceFromTop && scrollTop <= scrollDistance) {
+        document.querySelector(".element-container").style.transform = 
+        'translateX(-'+(scrollTop - distanceFromTop) + "px)";
+    }
+
+}
