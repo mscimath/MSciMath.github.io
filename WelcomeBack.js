6bb073e159e95aefdbc7d65e6baebd6bb08edef9
mscimath <madejscimath@gmail.com>
2023-02-19 04:12:56 +0100

    testing react components

---
+++ WelcomeBack.js
@@ -0,0 +1,23 @@
+class WelcomeBack extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: 'J',
+            appVersion: '1'
+        }
+    }
+
+    render(){
+        return(
+            <>
+                <h2>Hello {this.state.name || 'Friend'}! Welcome Back.</h2>
+                {
+                    this.state.appVersion && this.state.appVersion < 2
+                    ? <p>Your app is out of date. Please download the new version.</p>
+                    : ''
+                }
+                <CoolButton customText="Download now"/>
+            </>
+        )
+    }
+}
