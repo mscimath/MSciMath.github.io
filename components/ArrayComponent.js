14bb42207f780e92a9c3b27ad0ff898005c97255
mscimath <madejscimath@gmail.com>
2023-02-21 13:18:08 +0100

    created table react component

--- components/ArrayComponent.js
+++ components/ArrayComponent.js
@@ -1,3 +1,75 @@
+var objects = [{
+    objA : {
+    age: "8",
+    city: "Porto"
+    },
+    objB : {
+        age: "6",
+        city: "Paris",
+        experience: "1"
+      }
+}]
+
+var objC = Object.assign({}, objects.objA, objects.objB); 
+console.log(objC.age, objC.city, objC.experience);
+
+var contents = [   
+    {
+        table:
+            [
+                {
+                    unit: "Variable Declarations and Their Types",
+                    subjects: [
+                        {
+                            subject: "Creating Variables in JavaScript",
+                            href: "#"
+                        }, 
+                        {
+                            subject: "Data Types in Javascript",
+                            href: "#"
+                        }
+                    ]
+                },
+                {
+                    unit: "Types Conversion",
+                    subjects: [
+                        {
+                            subject: "Explicit and Implicit Conversion"
+                        }
+                    
+                    ]
+                }
+            ]
+    },
+    {
+        table:
+            [
+                {
+                    unit: "Variable Declarations and Their Types",
+                    subjects: [
+                        {
+                            subject: "Creating Variables in JavaScript",
+                            href: "#"
+                        }, 
+                        {
+                            subject: "Data Types in Javascript",
+                            href: "#"
+                        }
+                    ]
+                },
+                {
+                    unit: "Types Conversion",
+                    subjects: [
+                        {
+                            subject: "Explicit and Implicit Conversion"
+                        }
+                    
+                    ]
+                }
+            ]
+    }
+]
+
 class ArrayComponent extends React.Component {
     constructor(props){
         super(props);
@@ -10,7 +82,26 @@ class ArrayComponent extends React.Component {
     render(){
         return(
             <>
-            <p></p>
+                <div className="section-content">                   
+                        {contents.map((item) => 
+                        <table key={item.id} className="theory-subjects">  
+                            {item.table.map((subitem) =>
+                                <>
+                                    <tr>
+                                        <th>{subitem.unit}</th>
+                                    </tr>
+                                    {subitem.subjects.map((subject) =>
+                                        <tr key={subject.id}>
+                                            <td>
+                                                {subject.subject}
+                                            </td>
+                                        </tr>
+                                    )}
+                                </>
+                            )}                            
+                        </table>   
+                        )}                    
+                </div>
             </>
         )
     }
--- components/TitleAddition.js
+++ components/TitleAddition.js
@@ -1,26 +1,3 @@
-const names = [    
-    'Ada Lovelace', 
-    'Grace Hopper', 
-    'Margaret Hamilton'];
-var objects = [
-    {
-        age: "8",
-        city: "Porto"
-    }
-]
-var objA = {
-    age: "8",
-    city: "Porto"
-};
-var objB = {
-  age: "6",
-  city: "Paris",
-  experience: "1"
-};
-var objC = Object.assign({}, objA, objB); 
-console.log(objects.objD);
-  
-
 class TitleAddition extends React.Component {
     constructor(props) {
         super(props);
@@ -35,16 +12,7 @@ class TitleAddition extends React.Component {
             <>
                 <h2>S<span>cience & { `${this.props.name}` }
                 </span>M<span>athematics </span>B<span>its and </span>
-                P<span>ieces</span></h2>
-                <div>
-                    <div>
-                        {objects.map((object) => <p key={object.id}>{object.age}</p>)}
-                    </div>
-                    <div>
-                        {names.map((d) => <p key={d.id}>{d}</p>)}
-                    </div>
-                </div>
-                    
+                P<span>ieces</span></h2>                                    
             </>
         )
     }
